6abd6467f3566399706ef1befe79603070f169fb
vic6 <vicmatthews116@gmail.com>
2017-10-22 01:16:26 -0700

    Add showcard function

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import preload from "../data.json";
 
-const Search = () => <h1>I am the boss hog</h1>;
+const Search = () => (
+  <div className="search">
+    {preload.shows.map(show => (
+      <div className="show-card">
+        <img
+          alt={`${show.title} Show Poster`}
+          src={`/public/img/posters/${show.poster}`}
+        />
+        <h3>{show.title}</h3>
+        <h4>({show.year})</h4>
+        <p>{show.description}</p>
+      </div>
+    ))}
+  </div>
+);
 
 export default Search;
