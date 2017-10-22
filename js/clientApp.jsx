ac64bb286cc57fcd8c0e02ad5bead5e64ad818a6
vic6 <vicmatthews116@gmail.com>
2017-10-21 20:32:32 -0700

    Reactor client app to use jsx, add esling ignore

--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -4,21 +4,33 @@ import { render } from "react-dom";
 const ce = React.createElement;
 
 const MyTitle = function(props) {
-  return ce(
-    "div",
-    null,
-    ce("h1", { style: { color: props.color } }, props.title),
+  const style = { color: props.color };
+  return (
+    <div>
+      <h1 style={style}>{props.title}</h1>
+    </div>
   );
 };
 
+// class MyTitle {
+//   render() {
+//     const color = {props.color}
+//     return (
+//       <div>
+//         <h1 style={}>{props.title}</h1>
+//       </div>
+//     );
+//   }
+// }
+
 const MyFirstComponent = function() {
-  return ce(
-    "div",
-    null,
-    ce(MyTitle, { title: "Sauce", color: "blue" }),
-    ce(MyTitle, { title: "carn", color: "Peru" }),
-    ce(MyTitle, { title: "paper", color: "medium-aqua-marine" }),
-    ce(MyTitle, { title: "Trap", color: "Lime" }),
+  return (
+    <div id="my-first-component">
+      <MyTitle title="Carn Life" color="Peru" />
+      <MyTitle title="Carn Life" color="blue" />
+      <MyTitle title="Carn Life" color="green" />
+      <MyTitle title="Carn Life" color="orange" />
+    </div>
   );
 };
 
