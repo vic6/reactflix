3aab3021f073590ddad634e0ecb21ebaf925bf39
vic6 <vicmatthews116@gmail.com>
2017-10-21 19:27:42 -0700

    change eslintrc to fix errors

--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -1,25 +1,28 @@
+import React from "react";
+import { render } from "react-dom";
+
 const ce = React.createElement;
 
 const MyTitle = function(props) {
   return ce(
     "div",
     null,
-    ce("h1", { style: { color: props.color } }, props.title)
+    ce("h1", { style: { color: props.color } }, props.title),
   );
 };
 
-const MyFirstComponent = function() {
+const MyFirstComponent = function trap() {
   return ce(
     "div",
     null,
     ce(MyTitle, { title: "Sauce", color: "blue" }),
     ce(MyTitle, { title: "carn", color: "Peru" }),
     ce(MyTitle, { title: "paper", color: "medium-aqua-marine" }),
-    ce(MyTitle, { title: "Trap", color: "Lime" })
+    ce(MyTitle, { title: "Trap", color: "Lime" }),
   );
 };
 
 ReactDOM.render(
   React.createElement(MyFirstComponent),
-  document.getElementById("app")
+  document.getElementById("app"),
 );
