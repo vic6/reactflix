db2ee0bae45a7cbe65eed4a1cb26364e9885f60f
vic6 <vicmatthews116@gmail.com>
2017-10-22 23:57:34 -0700

    Change name to vixflix, add award winning movie

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Landing = () => (
   <div className="landing">
-    <h1>Fun Video 4 U</h1>
+    <h1>VicFlix</h1>
     <input type="text" placeholder="search" />
     <Link to="/search">or Browse all </Link>
   </div>
