f6de3c745dc29af979f5b06074d5072827bdc13c
vic6 <vicmatthews116@gmail.com>
2017-10-22 23:30:42 -0700

    Add styled showcard and image

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import { shape, string } from "prop-types";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 32%;
+  border: 2px solid #333;
+  border-radius: 4px;
+  margin-bottom: 25px;
+  padding-right: 10px;
+  overflow: hidden;
+`;
+
+const Image = styled.img`
+  width: 46%;
+  float: left;
+  margin-right: 10px;
+`;
 
 const ShowCard = props => (
-  <div className="show-card">
-    <img
+  <Wrapper>
+    <Image
       alt={`${props.show.title} Show Poster`}
       src={`/public/img/posters/${props.show.poster}`}
     />
     <h3>{props.show.title}</h3>
     <h4>({props.show.year})</h4>
     <p>{props.show.description}</p>
-  </div>
+  </Wrapper>
 );
 
 ShowCard.propTypes = {
