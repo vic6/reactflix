e2e65781a53f1d2948fe30fe46b89b8c2080d191
vic6 <vicmatthews116@gmail.com>
2017-10-23 23:07:37 -0700

    Add state to search component

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Landing = () => (
   <div className="landing">
-    <h1>VicFlix</h1>
+    <h1>VicsFlix</h1>
     <input type="text" placeholder="search" />
     <Link to="/search">or Browse all </Link>
   </div>
--- js/Search.jsx
+++ js/Search.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import preload from "../data.json";
 import ShowCard from "./ShowCard";
 
-const Search = () => (
-  <div className="search">
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.imdbID} show={show} />)}
-    </div>
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: "I love ice cream",
+    };
+  }
+
+  // Public class field, this is a stage 2 proposal
+  handleSearchTerm = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>{this.state.searchTerm}</h1>
+          <input
+            onChange={this.handleSearchTerm}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+
+        <div>
+          {preload.shows.map(show => (
+            <ShowCard key={show.imdbID} show={show} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
