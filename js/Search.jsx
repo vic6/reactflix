0fcd9c52105c1961c154eda5a046f5dbed8664a9
vic6 <vicmatthews116@gmail.com>
2017-10-24 00:23:07 -0700

    Add search filter

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,33 +3,34 @@ import preload from "../data.json";
 import ShowCard from "./ShowCard";
 
 class Search extends Component {
+  // Public class field, this is a stage 2 proposal, include in babelrc
+  // "plugins": ["babel-plugin-transform-class-properties"]
   state = {
-    searchTerm: "Hello",
+    searchTerm: "",
   };
+  handleSearchTerm = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  // Otherwise you can do it this way
+  // example without public class field
   // constructor(props) {
   //   super(props);
-  //   // alternate way of binding state
+  //   // alternate way of binding this
   //   // this.handleSearchTerm = this.handleSearchTerm.bind(this);
   //   this.state = {
   //     searchTerm: "I love ice cream",
   //   };
   // }
-
-  // example without public class field
   // handleSearchTerm(event) {
   //   this.setState(event.target.value)
   // }
-
-  // Public class field, this is a stage 2 proposal
-  handleSearchTerm = event => {
-    this.setState({ searchTerm: event.target.value });
-  };
 
   render() {
     return (
       <div className="search">
         <header>
-          <h1>{this.state.searchTerm}</h1>
+          <h1>VicsFlix</h1>
           <input
             onChange={this.handleSearchTerm}
             value={this.state.searchTerm}
@@ -39,9 +40,16 @@ class Search extends Component {
         </header>
 
         <div>
-          {preload.shows.map(show => (
-            <ShowCard key={show.imdbID} show={show} />
-          ))}
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0,
+            )
+            .map((show, index) => (
+              <ShowCard key={show.imdbID} show={show} id={index} />
+            ))}
         </div>
       </div>
     );
